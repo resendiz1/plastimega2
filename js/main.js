7f3830abed0befe4ebe2aaccc348631dfc71d459
Arturo Resendiz <resendizdeoz@hotmail.com>
2025-05-25 22:21:19 -0600

    faltan detalles

--- js/main.js
+++ js/main.js
@@ -282,51 +282,7 @@ document.addEventListener("keyup", function(e) {
 
 
 
-// Este es el js de el catalogo
-{
 
-let mostrador = document.getElementById("mostrador");
-let seleccion = document.getElementById("seleccion");
-let imgSeleccionada = document.getElementById("img");
-let modeloSeleccionado = document.getElementById("modelo");
-let descripSeleccionada = document.getElementById("descripcion");
-let precioSeleccionado = document.getElementById("precio");
-
-function cargar(item){
-    quitarBordes();
-    mostrador.style.width = "100%";
-    seleccion.style.width = "100%";
-    seleccion.style.height="50%"
-    seleccion.style.opacity = "1";
-    item.style.border = "2px solid rgb(16, 34, 99)";
-
-    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
-
-    modeloSeleccionado.innerHTML =  item.getElementsByTagName("p")[0].innerHTML;
-
-
-    precioSeleccionado.innerHTML =  item.getElementsByTagName("span")[0].innerHTML;
-
-
-}
-function cerrar(){
-    mostrador.style.width = "100%";
-    seleccion.style.width = "0%";
-    seleccion.style.opacity = "0";
-    quitarBordes();
-}
-function quitarBordes(){
-    var items = document.getElementsByClassName("item");
-    for(i=0;i <items.length; i++){
-        items[i].style.border = "none";
-    }
-}
-
-
-
-
-
-}
 
 
 {
